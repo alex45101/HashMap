ed338e45ec13f57f8527984f85bbfbeb726efc87
Alexander Leontiev <aleontiev00@gmail.com>
2018-08-21 15:45:43 -0700

    setting up hashmap class

--- ConsoleApp1/ConsoleApp1/HashMap.cs
+++ ConsoleApp1/ConsoleApp1/HashMap.cs
@@ -10,24 +10,41 @@ namespace ConsoleApp1
     public class HashMap<TKey, TValue> : IDictionary<TKey, TValue>
     {
         LinkedList<KeyValuePair<TKey, TValue>>[] buckets;
+        const int defaultCapacity = 30;
 
         public TValue this[TKey key] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public IEqualityComparer<TKey> EqualityComparer { get; private set; }
 
         public ICollection<TKey> Keys => throw new NotImplementedException();
 
         public ICollection<TValue> Values => throw new NotImplementedException();
 
-        public int Count => throw new NotImplementedException();
+        public int Count { get; private set; }
 
         public bool IsReadOnly => false;
 
         public HashMap()
-            : this(30)
-        {}
+            : this(defaultCapacity, null)
+        { }
 
         public HashMap(int capacity)
+            : this(capacity, null)
+        { }
+
+        public HashMap(IEqualityComparer<TKey> equalityComparer)
+            : this(defaultCapacity, equalityComparer)
+        { }
+
+        public HashMap(int capacity, IEqualityComparer<TKey> equalityComparer)
         {
+            if (capacity < 0)
+            {
+                throw new ArgumentOutOfRangeException("Capacity is less than zero");
+            }
+
             buckets = new LinkedList<KeyValuePair<TKey, TValue>>[capacity];
+            EqualityComparer = equalityComparer;
         }
 
         public void Add(TKey key, TValue value)
@@ -60,6 +77,7 @@ namespace ConsoleApp1
             throw new NotImplementedException();
         }
 
+        //TODO: Fix
         public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
         {
             for (int i = 0; i < Count; i++)
